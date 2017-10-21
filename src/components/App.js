14bbe0b7a5c5d977a82d5b570d36e184173a0086
smoisset <smoisset@worldremit.com>
2017-10-21 14:30:29 +0100

    Create AgeStats Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react'
 import { Form, FormControl, Button } from 'react-bootstrap'
+import AgeStats from './AgeStats'
 import './App.css'
 
 class App extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            newDate: '',
+            birthday: '1984-02-10'
+        }
+    }
+
+    changeBirthday() {
+        this.setState({ birthday: this.state.newDate })
+    }
+
     render() {
         return (
             <div className="App">
                 <Form inline>
                     <h2>Input Your Birthday!</h2>
-                    <FormControl type="date">
+                    <FormControl
+                        type="date"
+                    onChange={ event => this.setState({ newDate: event.target.value }) }>
                     </FormControl>
                     {' '}
-                    <Button>
+                    <Button onClick={ () => this.changeBirthday() }>
                         Submit
                     </Button>
+                    <AgeStats date={this.state.birthday} />
                 </Form>
             </div>
         )
